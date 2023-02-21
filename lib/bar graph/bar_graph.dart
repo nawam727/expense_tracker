9cf253d528be765da2f982caaa7182ce2981871f
nawam727 <nawamdenawakage@gmail.com>
2023-02-21 20:23:32 +0530

    Modify the bar graph

--- lib/bar graph/bar_graph.dart
+++ lib/bar graph/bar_graph.dart
@@ -38,10 +38,23 @@ class MyBarGraph extends StatelessWidget {
     );
     myBarData.intializeBarData();
 
-
     return BarChart(BarChartData(
       maxY: maxY,
       minY: 0,
+      titlesData: FlTitlesData(
+        show: true,
+        topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            getTitlesWidget: getBottomTitles,
+          ),
+        ),
+      ),
+      gridData: FlGridData(show: false),
+      borderData: FlBorderData(show: false),
       barGroups: myBarData.barData
           .map(
             (data) => BarChartGroupData(
@@ -49,11 +62,59 @@ class MyBarGraph extends StatelessWidget {
               barRods: [
                 BarChartRodData(
                   toY: data.y,
+                  color: Colors.grey[800],
+                  width: 25,
+                  borderRadius: BorderRadius.circular(4),
+                  backDrawRodData: BackgroundBarChartRodData(
+                    show: true,
+                    toY: maxY,
+                    color: Colors.grey[200],
                   ),
+                ),
               ],
-            ) ,
+            ),
           )
           .toList(),
     ));
   }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta) {
+  const style = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text('M', style: style);
+      break;
+    case 1:
+      text = const Text('T', style: style);
+      break;
+    case 2:
+      text = const Text('W', style: style);
+      break;
+    case 3:
+      text = const Text('T', style: style);
+      break;
+    case 4:
+      text = const Text('F', style: style);
+      break;
+    case 5:
+      text = const Text('S', style: style);
+      break;
+    case 6:
+      text = const Text('S', style: style);
+      break;
+    default:
+      text = const Text('', style: style);
+      break;
+  }
+
+  return SideTitleWidget(
+    child: text,
+    axisSide: meta.axisSide,
+  );
 }
--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/bar%20graph/bar_graph.dart';
 import 'package:expense_tracker/data/expense_data.dart';
+import 'package:expense_tracker/datetime/date_time_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,18 +14,26 @@ class ExpenseSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // get yyyymmdd for each day of this week
+    String monday = convertDateTimeToString(startOfWeek.add(const Duration(days: 0)));
+    String tuesday = convertDateTimeToString(startOfWeek.add(const Duration(days: 1)));
+    String wednesday = convertDateTimeToString(startOfWeek.add(const Duration(days: 2)));
+    String thursday = convertDateTimeToString(startOfWeek.add(const Duration(days: 3)));
+    String friday = convertDateTimeToString(startOfWeek.add(const Duration(days: 4)));
+    String saturday = convertDateTimeToString(startOfWeek.add(const Duration(days: 5)));
+    String sunday = convertDateTimeToString(startOfWeek.add(const Duration(days: 6)));
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
         height: 200,
         child: MyBarGraph(
-          maxY: 100, 
-          monAmount: 30, 
-          tueAmount: 40, 
-          wedAmount: 34, 
-          thuAmount: 4, 
-          friAmount: 67, 
-          satAmount: 93, 
-          sunAmount: 11,
+          maxY: 100,
+          monAmount: value.calculateDailyExpenseSummery()[monday] ?? 0,
+          tueAmount: value.calculateDailyExpenseSummery()[tuesday] ?? 0,
+          wedAmount: value.calculateDailyExpenseSummery()[wednesday] ?? 0,
+          thuAmount: value.calculateDailyExpenseSummery()[thursday] ?? 0,
+          friAmount: value.calculateDailyExpenseSummery()[friday] ?? 0,
+          satAmount: value.calculateDailyExpenseSummery()[saturday] ?? 0,
+          sunAmount: value.calculateDailyExpenseSummery()[sunday] ?? 0,
         ),
       ),
     );
